1bac3a59ccf8b9cb7af5f6e03a18c7b16a77818c
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-10-21 12:47:12 +0800

    added heuristic basis values

--- Place.java
+++ Place.java
@@ -1,16 +1,37 @@
 public class Place {
     final public String name;
     final public String code;
+
+    final public int price;
+    final public int time;
+    final public int s; // TODO: remember wtf this means
+    final public int p; // TODO: remember wtf this means
+    final public int accessibility;
+
     private double heuristic;
 
-    public Place(String name, String code) {
+    public Place(String name, String code, String PTSPA) {
         this.name = name;
         this.code = code;
+
+        String[] PTSPAArray = PTSPA.split("");
+        this.price = Integer.parseInt(PTSPAArray[0]);
+        this.time = Integer.parseInt(PTSPAArray[1]);
+        this.s = Integer.parseInt(PTSPAArray[2]);
+        this.p = Integer.parseInt(PTSPAArray[3]);
+        this.accessibility = Integer.parseInt(PTSPAArray[4]);
     }
 
     public Place(Place place) {
         this.name = place.name;
         this.code = place.code;
+
+        this.price = place.price;
+        this.time = place.time;
+        this.s = place.s;
+        this.p = place.p;
+        this.accessibility = place.accessibility;
+
         this.heuristic = place.heuristic;
     }
 
